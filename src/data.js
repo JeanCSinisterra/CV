63bfe3083085ddb7c1a9b0bc9e759a3fa241970d
JeanCSinisterra <84473537+JeanCSinisterra@users.noreply.github.com>
2022-12-10 00:13:24 -0500

    Adding these new files to my project

---
+++ src/data.js
@@ -0,0 +1,60 @@
+export const projects = [
+  {
+    title: "Honda Aire y Servicio",
+    subtitle: "Website | Business",
+    description:
+      "Car workshop`s website to address Honda car owners to appointments to repair their cars through an assistant with WhatsApp Business",
+    image: "./project-1.png",
+    link: "https://www.hondaaireyservicio.com/",
+  },
+  {
+    title: "Bryan Rojas Guitar Tech",
+    subtitle: "Website | Luthier",
+    description:
+      "Bryan Rojas is here to provide you with superior instrument repair and maintenance services.",
+    image: "./project-2.png",
+    link: "https://jeancsinisterra.github.io/Bryan-Rojas-Guitar-Tech/",
+  },
+  {
+    title: "Weather App",
+    subtitle: "Node & Express",
+    description:
+      "This project is based on the Open Weather Map to handle its API. Extract the Data and display to the user in a simple card.",
+    image: "./project-3.png",
+    link: "https://weather-app-pwko.onrender.com",
+  },
+  {
+    title: "Secret App",
+    subtitle: "Authentication & Security",
+    description:
+      "This repository was created to put in practice the Authentication & security basics concepts. You can watch the source code in the link.",
+    image: "./project-4.png",
+    link: "https://github.com/JeanCSinisterra/SecretsApp",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Since having our new website built by Jean, we have seen a 80% increase in the number of online contact forms being filled out and returned to us. Thanks to Jean for all of your hard work and support.",
+    image: "https://randomuser.me/api/portraits/men/1.jpg",
+    name: "Aaron De Leon",
+    company:"Marketing Expert",
+  },
+  {
+    quote:
+      "Definitely working with Jean was a pleasure to work with and we look forward to working with him again. He’s definitely the kind of developer you can trust with a project from start to finish.",
+    image: "https://randomuser.me/api/portraits/men/94.jpg",
+    name: "Ruben Vallarino",
+    company: "Lawyer",
+  },
+];
+
+export const skills = [
+  "JavaScript",
+  "React",
+  "Next.js",
+  "Node",
+  "MongoDB",
+  "Bootstrap",
+];
