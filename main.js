8b3ce2e9fd80c850660bd5aa57ab0266a283a575
JeanCSinisterra <84473537+JeanCSinisterra@users.noreply.github.com>
2022-12-09 22:15:13 -0500

    Delete Unnecessary JavaScript Code

--- main.js
+++ main.js
@@ -18,28 +18,3 @@ function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 };
-
-// In the Contact form, this script is to send Contact Enquiry directly to my Gmail Account. 
-document.getElementByClassName("test-btn").addEventListener("click", (e) => {
-  e.preventDefault();
-  const contactForm = document.getElementById("form");
-  const name = document.getElementById("name");
-  const myEmail = document.getElementById("email");
-  const myMessage = document.getElementById("message-form");
-  // validation before sending the data
-  if (
-    name.value.length === 0 ||
-    name.value.length === 0 ||
-    name.value.length === 0
-  ) {
-    alert("please fill the inputs");
-  } else {
-    let data = new FormData(contactForm);
-    fetch("https://formsubmit.co/jeansinisterra@gmail.com", {
-      method: "POST",
-      body: data,
-    });
-    alert("Thank you. your form was submited");
-    contactForm.reset();
-  }
-});
